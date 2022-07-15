d1dca0e1cfedbda534afb506c6c1234590c06a1f
Sebastian Vera <sebastian.vera@oktana.com>
2022-07-15 14:30:50 -0300

    Connected to DB and fixed port for heroku

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+  user: 'drugman',
+  host: 'unmtid-dbs.net',
+  database: 'drugcentral',
+  password: 'dosage',
+  port: 5433
+});
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`API Running on port ${port}`);
+});
+
+const inMemoryResponse = [
+    {
+      name: 'Neurontin',
+      lab: 'Pfizer',
+      active_ingredient: 'Gabapentin',
+      strength: '300',
+      Form: 'Capsules',
+    }
+  ];
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/api/', (req, res) => {
+    res.status(200).json(inMemoryResponse);
+});
+
+app.get('/api/query/product/:ndc_product_code', (req, res) => {
+    const ndc_product_code = req.params.ndc_product_code;
+    pool.query("SELECT generic_name, product_name, form FROM public.product WHERE ndc_product_code = $1", [ndc_product_code], (error, results) => {
+        res.status(200).json(results.rows);
+      });
+});
+
+module.exports = app
+
--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
 const request = require("supertest");
-const apptest = require("../app");
+const apptest = require("../index");
 
 describe("GET /", () => {
     it("responds with on memory mock!", (done) => {
         request(apptest).get("/api/").expect('[{"name":"Neurontin","lab":"Pfizer","active_ingredient":"Gabapentin","strength":"300","Form":"Capsules"}]', done);
+    })
+});
+describe("GET /", () => {
+    it("connects and query DB!", (done) => {
+        request(apptest).get("/api/query/product/65483-991").expect('[{"generic_name":"allopurinol","product_name":"ZYLOPRIM","form":"TABLET"}]', done);
     })
 });
