4b7a78924504bfc3e876f6839378cacf647cac6e
Sebastian Vera <sebastian.vera@oktana.com>
2022-07-19 17:23:16 -0300

    Fix Endpoint

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/api/query/generic/:generic_name', (req, res) => {
   const generic_name = req.params.generic_name;
 
   pool.query(`SELECT generic_name, product_name, ndc_product_code form FROM public.product WHERE generic_name like '%'||$1||'%'`, [generic_name], (error, results) => {
-      res.status(200).json(results.rows);
+      res.status(200).json(results.rows).set({'Content-Type':'application/json'});
     });
   
 });
