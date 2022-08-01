672497fcc149d55d6dcd0a77aa6093b85c4ed615
Sebastian Vera <sebastian.vera@oktana.com>
2022-08-01 15:46:46 -0300

    Add product end point and test it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 const Pool = require('pg').Pool;
 const cors = require('cors');
@@ -41,14 +41,6 @@ app.get('/api/', (req, res) => {
     res.status(200).json(inMemoryResponse);
 });
 
-app.get('/api/query/product/:ndc_product_code', (req, res) => {
-    const ndc_product_code = req.params.ndc_product_code;
-    pool.query("SELECT generic_name, product_name, form FROM public.product WHERE ndc_product_code = $1", [ndc_product_code], (error, results) => {
-        res.status(200).json(results.rows);
-      });
-});
-
-
 app.get('/api/query/generic/:generic_name', (req, res) => {
   const generic_name = req.params.generic_name;
 
@@ -60,6 +52,22 @@ app.get('/api/query/generic/:generic_name', (req, res) => {
   INNER JOIN public.structures AS struct ON struct.id = active_ingredient.struct_id
   WHERE generic_name like '%'||$1||'%'
   ORDER BY lab.assigned_entity ASC`, [generic_name], (error, results) => {
+      res.set({'Content-Type':'application/json'}).status(200).json(results.rows);
+    });
+  
+});
+
+app.get('/api/query/product/:product_name', (req, res) => {
+  const product_name = req.params.product_name;
+
+  pool.query(`SELECT product.product_name, product.generic_name, product.form, product.ndc_product_code, struct.mrdef, lab.assigned_entity, lab.id, active_ingredient.quantity, active_ingredient.unit
+  FROM public.product AS product
+  INNER JOIN public.prd2label AS plabel ON plabel.ndc_product_code = product.ndc_product_code 
+  INNER JOIN public.label AS lab ON lab.id = plabel.label_id
+  INNER JOIN public.active_ingredient AS active_ingredient ON active_ingredient.ndc_product_code = product.ndc_product_code
+  INNER JOIN public.structures AS struct ON struct.id = active_ingredient.struct_id
+  WHERE product_name like '%'||$1||'%'
+  ORDER BY lab.assigned_entity ASC`, [product_name], (error, results) => {
       res.set({'Content-Type':'application/json'}).status(200).json(results.rows);
     });
   
--- test/test.js
+++ test/test.js
@@ -10,11 +10,26 @@ describe("GET /", () => {
     })
 });
 
-describe("DB Calls", function () {
+describe("DB Get Generics", function () {
     describe("GET /", function () {
       it("should return 200 OK with several generic drugs", async function () {
         const response = await request(apptest)
           .get("/api/query/generic/Quinapril")
+          .expect(200)
+          .expect("Content-Type", /json/);
+  
+        const drugs = response.body;
+        expect(drugs).to.be.an("array");
+        expect(drugs).length.to.be.greaterThan(0);
+      });
+    });
+  });
+
+  describe("DB Get Products", function () {
+    describe("GET /", function () {
+      it("should return 200 OK with several products", async function () {
+        const response = await request(apptest)
+          .get("/api/query/product/Quetiapine")
           .expect(200)
           .expect("Content-Type", /json/);
   
